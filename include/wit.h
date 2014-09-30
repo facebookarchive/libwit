8789c2115566fa70e48ddd8c342db632ba48d784
Martin Raison <martinraison@gmail.com>
2014-09-30 08:43:15 +0200

    removing const qualifiers in headers file

--- include/wit.h
+++ include/wit.h
@@ -4,16 +4,16 @@
 #include <stdlib.h>
 
 struct wit_context;
-typedef void (*wit_resp_callback)(const char *);
+typedef void (*wit_resp_callback)(char *);
 
 struct wit_context *wit_init(const char *device_opt);
 void wit_close(struct wit_context *context);
-const char *wit_text_query(struct wit_context *context, const char *text, const char *access_token);
+char *wit_text_query(struct wit_context *context, const char *text, const char *access_token);
 void wit_text_query_async(struct wit_context *context, const char *text, const char *access_token, wit_resp_callback cb);
-const char *wit_voice_query_auto(struct wit_context *context, const char *access_token);
+char *wit_voice_query_auto(struct wit_context *context, const char *access_token);
 void wit_voice_query_auto_async(struct wit_context *context, const char *access_token, wit_resp_callback cb);
 void wit_voice_query_start(struct wit_context *context, const char *access_token);
-const char *wit_voice_query_stop(struct wit_context *context);
+char *wit_voice_query_stop(struct wit_context *context);
 void wit_voice_query_stop_async(struct wit_context *context, wit_resp_callback cb);
 
 #endif
