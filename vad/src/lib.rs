0aaa70cc7374d7d8f1709e25d347252d50af3f47
Martin Raison <martinraison@gmail.com>
2014-12-04 11:11:35 +0100

    linking bug: putting vad in its own crate

---
+++ vad/src/lib.rs
@@ -0,0 +1,9 @@
+extern crate libc;
+
+use libc::{c_int, c_double, c_void};
+
+extern {
+    pub fn wvs_still_talking(state: *const c_void, samples: *const i16, nb_samples: c_int) -> c_int;
+    pub fn wvs_init(threshold: c_double, sample_rate: c_int) -> *const c_void;
+    pub fn wvs_clean(state: *const c_void);
+}
