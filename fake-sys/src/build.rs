9e5336802634705292a27c43051fefb5e2a0cd96
Martin Raison <martinraison@gmail.com>
2014-12-04 15:36:24 +0100

    faking __cxa_thread_atexit_impl

--- fake-sys/src/build.rs
+++ fake-sys/src/build.rs
@@ -1,4 +1,17 @@
+use std::os;
+use std::io::{File,Command};
+
 fn main() {
-    // TODO: compile the library here using the cross-compiling toolchain
-    println!("cargo:rustc-flags=-L lib/arm -l fake:static");
+    let out_dir = os::getenv("OUT_DIR").unwrap();
+    let dst = Path::new(out_dir.clone());
+
+    let mut process = match Command::new("./build.sh").spawn() {
+      Ok(p) => p,
+      Err(e) => panic!("failed to execute process: {}", e),
+    };
+    let output = process.stdout.as_mut().unwrap().read_to_string()
+        .ok().expect("failed to read libfake build output");
+    let mut f = File::create(&dst.join("gen_fake_log.txt")).unwrap();
+    f.write_str(output.as_slice()).unwrap();
+    println!("cargo:rustc-flags=-L {} -l fake:static", out_dir);
 }
